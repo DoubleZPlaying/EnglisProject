39cc5b27cb0089c66267ed25c1ad8edfeb3169da
DoubleZPlaying <zevieywonder@gmail.com>
2022-03-16 20:17:30 -0700

    starter

---
+++ Choices.java
@@ -0,0 +1,108 @@
+public class Choices
+{
+    String wakeUp;
+    String atSchool;
+    String bonus;
+    String arriveToClass;
+    String lunch;
+    String test;
+    String endSchool;
+    String homework;
+    String sleep;
+    int day;
+
+    public Choices(int day)
+    {
+        this.day = day;
+    }
+
+    public void setWakeUp(String wakeUp)
+    {
+        this.wakeUp = wakeUp;
+    }
+
+    public void getWakeUp()
+    {
+
+    }
+
+    public void setAtSchool(String atSchool)
+    {
+        this.atSchool = atSchool;
+    }
+
+    public void getAtSchool()
+    {
+        
+    }
+
+    public void setBonus(String bonus)
+    {
+        this.bonus = bonus;
+    }
+
+    public void getBonus()
+    {
+
+    }
+
+    public void setArriveToClass(String arriveToClass)
+    {
+        this.arriveToClass = arriveToClass;
+    }
+
+    public void getArriveToClass()
+    {
+
+    }
+
+    public void setLunch(String lunch)
+    {
+        this.lunch = lunch;
+    }
+
+    public void getLunch()
+    {
+
+    }
+
+    public void setTest(String test)
+    {
+        this.test = test;
+    }
+
+    public void getTest()
+    {
+
+    }
+
+    public void setEndSchool(String endSchool)
+    {
+        this.endSchool = endSchool;
+    }
+
+    public void getEndSchool()
+    {
+        
+    }
+
+    public void setHomework(String homework)
+    {
+        this.homework = homework;
+    }
+
+    public void getHomework()
+    {
+
+    }
+
+    public void setSleep(String sleep)
+    {
+        this.sleep = sleep;
+    }
+
+    public void getSleep()
+    {
+
+    }
+}
--- main.java
+++ main.java
@@ -7,13 +7,19 @@ public class main
         Scanner input = new Scanner(System.in);
         String temp;
 
-        System.out.println("It's a normal day. You wake up to your alarm. Do you ignore it, or wake up? (ignore, wake up)");
-        temp = input.nextLine();
-        temp = temp.toLowerCase(); 
+        Choices day = new Choices(1);
 
-        if(temp.equals("wake up"))
-        {
-            
-        }
+        while(true){
+
+            System.out.println("It's a normal day. You wake up to your alarm. Do you ignore it, or wake up? (ignore, wake up)");
+            temp = input.nextLine();
+            temp = temp.toLowerCase(); 
+            System.out.println();
+
+            if(temp.equals("wake up"))
+            {
+                day.setWakeUp(temp);
+            }
+        }   
     }
 }

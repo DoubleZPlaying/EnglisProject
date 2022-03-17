d1477ae38cb29362160e838a05155299baf4d914
DoubleZPlaying <80644959+DoubleZPlaying@users.noreply.github.com>
2022-03-16 19:51:50 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class main
+{
+    public static void main(String[] args)
+    {
+        //philosophy: Your fate is altered by the choices you make using free will
+        Scanner input = new Scanner(System.in);
+        String temp;
 
+        System.out.println("It's a normal day. You wake up to your alarm. Do you ignore it, or wake up? (ignore, wake up)");
+        temp = input.nextLine();
+        temp = temp.toLowerCase(); 
+
+        if(temp.equals("wake up"))
+        {
+            
+        }
+    }
+}
